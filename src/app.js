ee42c4b15a63e7ee3eee6fa3a25dc9ce5ef1b72b
Sierrena Taylor-Seals <sierrena.taylor@outlook.com>
2025-06-15 16:09:46 +0000

    random card generator finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let card = document.querySelector(".card"); 
+  let cardNumber = document.querySelector(".card-number");
+  let suit = ["heart", "spade", "diamond", "club"];
+  let value = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function generateCard() {
+    let randomSuit = suit[Math.floor(Math.random() * suit.length)];
+    let randomValue = value[Math.floor(Math.random() * value.length)];
+
+    card.classList.add(randomSuit);
+    cardNumber.textContent = randomValue;
+
+    if (randomSuit === "♥" || randomSuit === "♦") {
+      card.querySelector(".suit.top").style.color = "red";
+      card.querySelector(".suit.bottom").style.color = "red";
+    } else {
+      card.querySelector(".suit.top").style.color = "black";
+      card.querySelector(".suit.bottom").style.color = "black";
+    }
+  }
+
+  generateCard();
+  
+}
